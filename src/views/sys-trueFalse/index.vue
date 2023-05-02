b2db89b9098a71da7341a7f4dd71aa994f5cdcb7
xiaozhangtongx <2446159171@qq.com>
2023-05-02 22:22:06 +0800

    feat(sys-trueFalse): finish sys-trueFalse page

--- src/views/sys-trueFalse/index.vue
+++ src/views/sys-trueFalse/index.vue
@@ -16,8 +16,8 @@ const createTime = useDateFormat(new Date(), 'YYYY-MM-DD HH:mm:ss').value
 
 const singleChoiceForm = reactive<IApiQuestionInfoData>({
   id: uuid,
-  title: '您的学校是',
-  type: 1,
+  title: '你喜欢加班吗',
+  type: 0,
   score: 10,
   answerId: '',
   sort: 0,
@@ -26,7 +26,7 @@ const singleChoiceForm = reactive<IApiQuestionInfoData>({
   sysQuestionItemList: [
     {
       id: getUuid(),
-      content: '武汉大学',
+      content: '喜欢',
       questionId: uuid,
       sort: '1',
       des: '',
@@ -34,25 +34,9 @@ const singleChoiceForm = reactive<IApiQuestionInfoData>({
     },
     {
       id: getUuid(),
-      content: '武汉理工大学',
+      content: '不喜欢',
       questionId: uuid,
       sort: '2',
-      des: '',
-      createTime,
-    },
-    {
-      id: getUuid(),
-      content: '华中科技大学',
-      questionId: uuid,
-      sort: '3',
-      des: '',
-      createTime,
-    },
-    {
-      id: getUuid(),
-      content: '华中师范大学',
-      questionId: uuid,
-      sort: '4',
       des: '',
       createTime,
     },
@@ -94,7 +78,7 @@ const rules = reactive<FormRules>({
 const submitForm = async (formEl: FormInstance | undefined) => {
   if (!formEl)
     return
-  await formEl.validate((valid, fields) => {
+  await formEl.validate((valid) => {
     loading.value = true
     if (valid) {
       addQuestionApi(singleChoiceForm)
@@ -116,24 +100,6 @@ const resetForm = (formEl: FormInstance | undefined) => {
     return
   formEl.resetFields()
 }
-
-// TODO: 添加题目选项
-const addQuestionItem = () => {
-  const questionItem = {
-    id: getUuid() as string,
-    content: '',
-    questionId: uuid,
-    sort: `${singleChoiceForm.sysQuestionItemList.length + 1}`,
-    des: '',
-    createTime,
-  }
-  singleChoiceForm.sysQuestionItemList.push(questionItem)
-}
-
-// TODO:移除问题选项
-const removeQuestionItem = (id: string) => {
-  singleChoiceForm.sysQuestionItemList = singleChoiceForm.sysQuestionItemList.filter(item => item.id !== id)
-}
 </script>
 
 <template>
@@ -151,14 +117,11 @@ const removeQuestionItem = (id: string) => {
         <el-form-item label="题干" prop="title">
           <el-input v-model="singleChoiceForm!.title" />
         </el-form-item>
-        <el-form-item label="题目选项" prop="sysQuestionItemList">
+        <el-form-item label="题目选项">
           <el-radio-group v-model="singleChoiceForm.sysQuestionItemList[0].id" class="flex flex-col">
             <li v-for="(questionItem, index) in singleChoiceForm?.sysQuestionItemList" :key="questionItem.id" class="m-0 flex ">
               <el-radio :label="index" size="large" disabled />
               <el-input v-model="questionItem.content" class="w-100 m-2" size="small" />
-              <el-button type="danger" class="ml-3 mt-2" size="small" @click="removeQuestionItem(questionItem.id)">
-                删除
-              </el-button>
             </li>
           </el-radio-group>
         </el-form-item>
@@ -190,9 +153,6 @@ const removeQuestionItem = (id: string) => {
           </el-button>
           <el-button @click="resetForm(singleChoiceFormRef)">
             重置
-          </el-button>
-          <el-button type="success" @click="addQuestionItem()">
-            添加选项
           </el-button>
           <el-button type="success" @click="dialogTableVisible = true">
             预览
